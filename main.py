928ab18e1af145ad4e2220fa3acb222787f73cc0
aticie <efehanatici@gmail.com>
2020-04-01 15:00:46 +0300

    Fix pingme returning id instead of username when used with id

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import io
 import os
 import sqlite3
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
 from discord.ext import commands
 import discord
 
@@ -19,14 +21,14 @@ with open("continents.csv", "r") as f:
 
 c = conn.cursor()
 # Create table
-c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users' ''')
+c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users_new' ''')
 
 # if the count is 1, then table exists
 if c.fetchone()[0] == 1:
     print('Table exists. Skipping new table creation.')
 else:
     print('Table does not exist. Creating new table.')
-    c.execute('''CREATE TABLE users (discord id, osu username, rank, bws_rank, country, last updated)''')
+    c.execute('''CREATE TABLE users_new (discord TYPE UNIQUE, osu, rank, bws_rank, country, last_updated)''')
     print('Created users table.')
 
 prefix = "?"
@@ -36,14 +38,14 @@ client = commands.Bot(command_prefix=prefix, case_insensitive=True)
 
 def find_user_in_db(discord_id):
     args = (discord_id,)
-    statement = 'SELECT * FROM users WHERE discord=?'
+    statement = 'SELECT * FROM users_new WHERE discord=?'
     c.execute(statement, args)
 
-    return c.fetchall()
+    return c.fetchone()
 
 
 def get_users_in_db():
-    c.execute('''SELECT * FROM users''')
+    c.execute('''SELECT * FROM users_new''')
 
     return c.fetchall()
 
@@ -56,7 +58,7 @@ def add_user_to_db(discord_id, osu_details):
     bws_rank = max(1, int(pow(user_rank, (pow(0.9937, pow(user_badges, 2))))))
     user_country = osu_details["country"]["name"]
     updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)",
+    c.execute("INSERT INTO users_new VALUES (?, ?, ?, ?, ?, ?)",
               (discord_id, osu_username, user_rank, bws_rank, user_country, updated))
     conn.commit()
 
@@ -65,7 +67,7 @@ def add_user_to_db(discord_id, osu_details):
 
 def delete_user_from_db(discord_id):
     discord_id = (discord_id,)
-    c.execute('''DELETE FROM users WHERE discord=?''', discord_id)
+    c.execute('''DELETE FROM users_new WHERE discord=?''', discord_id)
     conn.commit()
 
     return
@@ -78,12 +80,60 @@ async def on_command_error(self, *args, **kwargs):
     await self.send(args[0])
     return
 
+
+@client.command(name='showme')
+async def fix_bws_rank(ctx):
+    user = find_user_in_db(ctx.author.id)
+
+    if user is None:
+        await ctx.send(f"You are not registered as someone.")
+        return
+    else:
+        _, osu_name, rank, bws_rank, country, last_updated = user
+        embed = discord.Embed(description=f"**▸Rank:** {rank}\n"
+                                          f"**▸BWS Rank:** {bws_rank}\n"
+                                          f"**▸Country:** {country}\n",
+                              color=ctx.author.color)
+        embed.set_author(name=f"osu! profile for {osu_name}")
+        embed.set_footer(text=f"Profile last updated at: {last_updated}")
+        await ctx.send(embed=embed)
+    return
+
 @client.command(name='fixbws')
 @commands.has_permissions(administrator=True)
-async def fix_bws_rank(ctx, osu_username):
+async def fix_bws_rank(ctx, osu_username, user_badges: int):
     """
-    Fixes a user's bws rank. Not implemented yet.
+    Fixes a user's bws rank. `?fixbws heyronii 6` -> Sets heyronii's badges to 6.
+    osu_username: This parameter has to be exact same on osu!. Don't replace spaces with underscores or it won't work!
+    user_badges: Set to new amount of badges
     """
+    assert user_badges >= 0, 'User badges can\'t be negative.'
+    c.execute('SELECT * FROM users_new WHERE osu=?', (osu_username, ))
+    db_users = c.fetchall()
+
+    if len(db_users) == 0:
+        await ctx.send(f"I couldn't find anyone named `{osu_username}`")
+        return
+
+    osu_details, _ = get_osu_user_web_profile(osu_username)
+
+    osu_username = osu_details["username"]
+    user_rank = osu_details["statistics"]["pp_rank"]
+    # Standard bws rank = rank^(0.9937^(badges^2))
+    bws_rank = max(1, int(pow(user_rank, (pow(0.9937, pow(user_badges, 2))))))
+    updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    c.execute("UPDATE users_new SET rank=?, bws_rank=?, last_updated=? WHERE osu=?",
+              (user_rank, bws_rank, updated, osu_username))
+    conn.commit()
+
+    await ctx.send(f"Updated `{osu_username}`'s badges to {user_badges}. His new bws rank is: {bws_rank}")
+
+    return
+
+
+@client.event
+async def on_member_remove(member):
+    delete_user_from_db(member.id)
     return
 
 
@@ -94,7 +144,7 @@ async def tourney_ping_off(ctx):
     """
 
     user = find_user_in_db(ctx.author.id)
-    if len(user) == 0:
+    if user is None:
         await ctx.send(
             f"{ctx.author.mention} you are not even being notified.\n If you want to be notified use `{prefix}pingme`.")
         return
@@ -112,11 +162,17 @@ async def tourney_ping_on(ctx, osu_username):
     Usage: *pingme Cookiezi - (You will be notified when a tournament is announced that Cookiezi can join)
     osu_username: Your osu! nickname or id (If your name contains spaces, put it in quotation marks)
     """
-    user_discord_id = ctx.author.id
+    user = find_user_in_db(ctx.author.id)
+
+    if user is not None:
+        username = user[1]
+        await ctx.send(f"{ctx.author.mention} you are already registered as `{username}`. \n"
+                       f"If you registered accidentally, use `?pingmenot` and then `?pingme` again.")
+        return
 
     user_data, _ = get_osu_user_web_profile(osu_username)
     username = user_data["username"]
-    add_user_to_db(user_discord_id, user_data)
+    add_user_to_db(ctx.author.id, user_data)
 
     await ctx.send(
         f"{ctx.author.mention} you will be notified when a new tournament is announced for `{username}`!\n"
@@ -161,15 +217,15 @@ async def on_message(message):
 
         if len(regions) == 0:
             if bws:
-                c.execute("SELECT * FROM users WHERE bws_rank<? AND bws_rank>?", (min_rank, max_rank))
+                c.execute("SELECT * FROM users_new WHERE bws_rank<? AND bws_rank>?", (min_rank, max_rank))
             else:
-                c.execute("SELECT * FROM users WHERE rank<? AND rank>?", (min_rank, max_rank))
+                c.execute("SELECT * FROM users_new WHERE rank<? AND rank>?", (min_rank, max_rank))
         else:
             arguments = [min_rank, max_rank]
             if bws:
-                statement = "SELECT * FROM users WHERE bws_rank<=? AND bws_rank>=? "
+                statement = "SELECT * FROM users_new WHERE bws_rank<=? AND bws_rank>=? "
             else:
-                statement = "SELECT * FROM users WHERE rank<=? AND rank>=? "
+                statement = "SELECT * FROM users_new WHERE rank<=? AND rank>=? "
             for country in regions:
                 arguments.append(country)
                 statement += "AND country=? "
@@ -230,7 +286,7 @@ async def on_message(message):
     ping_text = ""
     if not ping_everyone:
         for user in ping_list:
-            ping_text += f" {client.get_user(user).mention}"
+            ping_text += f"<@{user}>"
     else:
         for role in everyone_roles:
             ping_text += f"{role.mention}"
@@ -245,7 +301,6 @@ async def on_message(message):
 
 
 def get_osu_user_web_profile(osu_username):
-    osu_username = osu_username.replace("_", " ")
     r = requests.get(f"https://osu.ppy.sh/users/{osu_username}")
 
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -253,7 +308,7 @@ def get_osu_user_web_profile(osu_username):
         json_user = soup.find(id="json-user").string
         json_achievements = soup.find(id="json-achievements").string
     except AttributeError:
-        raise Exception(f"Couldn't find `{osu_username}` on osu!")
+        raise Exception(f"Couldn't find `{osu_username}` on osu!\n Try using your id instead.")
     user_dict = json.loads(json_user)
     achievements_dict = json.loads(json_achievements)
 
